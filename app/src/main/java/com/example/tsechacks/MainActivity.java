771deb337e6eb2b27f243722c0c3ff036fe96825
BhaviDave <bhavidave5@gmail.com>
2020-02-05 01:13:26 +0530

    python integrated

--- app/src/main/java/com/example/tsechacks/MainActivity.java
+++ app/src/main/java/com/example/tsechacks/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.tsechacks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textview=findViewById(R.id.textView);
+
+        if (!Python.isStarted())
+        {
+            Python.start(new AndroidPlatform(this));
+        }
+
+        Python py=Python.getInstance();
+        PyObject pyf=py.getModule("pytest");
+        PyObject object=pyf.callAttr("test");
+        textview.setText(object.toString());
+
     }
 }
---
+++ app/src/main/python/pytest.py
@@ -0,0 +1,2 @@
+def test():
+    return "YOOOOO IT WORKS!"
